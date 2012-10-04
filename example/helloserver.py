028131571a9dc0d486ceb670285951dcfac3f1c7
INADA Naoki <inada-n@klab.com>
2012-10-05 08:43:22 +0900

    helloserver: リクエストの内容を表示する.

--- example/helloserver.py
+++ example/helloserver.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 from gevent.wsgi import WSGIServer
+import gevent
+import random
+import urlparse
 
 def app(env, start_response):
-    start_response("200 OK", [])
-    return ["Hello World"]
+    gevent.sleep(random.random() * 0.1)
+    if env['REQUEST_METHOD'] == 'POST':
+        data = env['wsgi.input'].read()
+        if env.get('CONTENT_TYPE') == 'application/x-www-form-urlencoded':
+            for k, v in urlparse.parse_qsl(data):
+                print "%s: %s" % (k, v)
+        else:
+            print data
+    res = 'Hello World'
+    header = [('Content-Length', str(len(res)))]
+
+    if env['PATH_INFO'].startswith('/redirect'):
+        header += [('Location', 'http://localhost:8889/Boom')]
+        start_response("302 Moved Temporary", header)
+    else:
+        start_response("200 OK", header)
+    return [res]
 
 server = WSGIServer(('', 8889), app)
 server.serve_forever()
